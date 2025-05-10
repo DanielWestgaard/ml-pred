9bbe3decd5077972307c4fb5f1ebc8b3c926a090
DanielWestgaard <daniel.westgaard@gmail.com>
2025-05-10 12:46:58 +0200

    bdf

--- data/features/selection.py
+++ data/features/selection.py
@@ -16,6 +16,21 @@ class FeatureSelector(BaseProcessor):
     
     def run(self):
         """Run the feature selection process."""
+
+        self.testing_selection()
+        print(len(self.df))
+        return self.df
+    
+    def _prepare_training_data(self, lookback=30):
+        """Simple method to get x- and y-train."""
+        X, y = [], []
+        for i in range(len(self.df) - lookback):
+            X.append(self.df.iloc[i:i+lookback]['close'].values)
+            y.append(self.df.iloc[i+lookback]['close'])
+        return np.array(X), np.array(y)
+    
+    def testing_selection(self):
+        """Temporary testing method for exploring different solutions."""
         X, y = self._prepare_training_data()
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -32,13 +47,3 @@ class FeatureSelector(BaseProcessor):
         plt.show()
         
         print(f"Model Score: {model.score(X_test, y_test)}")
-        
-        return self.df
-    
-    def _prepare_training_data(self, lookback=30):
-        """Simple method to get x- and y-train."""
-        X, y = [], []
-        for i in range(len(self.df) - lookback):
-            X.append(self.df.iloc[i:i+lookback]['close'].values)
-            y.append(self.df.iloc[i+lookback]['close'])
-        return np.array(X), np.array(y)
