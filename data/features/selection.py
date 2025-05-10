2608df6b3633527ab5f09ad0142815aff183d0c5
DanielWestgaard <daniel.westgaard@gmail.com>
2025-05-10 11:48:29 +0200

    FS class

---
+++ data/features/selection.py
@@ -0,0 +1,11 @@
+from data.processing.base_processor import BaseProcessor
+
+
+class FeatureSelector(BaseProcessor):
+    def __init__(self):
+        """Class for Selecting the most important features."""
+        pass
+    
+    def run(self):
+        """Run the feature selection process."""
+        pass
