947d7f5d2464c9525b08e87823859f8b235a6269
DanielWestgaard <daniel.westgaard@gmail.com>
2025-05-15 20:54:34 +0200

    removed One-hot encoding

--- data/features/transformation.py
+++ data/features/transformation.py
@@ -136,26 +136,6 @@ class FeatureTransformer(BaseProcessor):
                     
                     logging.info("Filled beginning NaNs with first valid values")
     
-    def encode_categorical_vars(self):
-        """Converting categorical/textual data into numerical format. Like One-hot encoding, label encoding."""
-        try:
-            categorical_columns = self.df.select_dtypes(include=['object']).columns.tolist()
-            logging.debug(f"Categorical columns about to be one-hot encoded: {categorical_columns}")
-            encoder = OneHotEncoder(sparse_output=False)
-
-            one_hot_encoded = encoder.fit_transform(self.df[categorical_columns])
-            one_hot_df = pd.DataFrame(
-                one_hot_encoded, 
-                columns=encoder.get_feature_names_out(categorical_columns),
-                index=self.df.index  # Both having date as index
-            )
-            df_encoded = pd.concat([self.df, one_hot_df], axis=1)
-
-            self.df = df_encoded.drop(categorical_columns, axis=1)
-            logging.debug(f"Encoded data successfully!")
-        except Exception as e:
-            logging.error("Unable to one-hot encode dataset!")
-    
     def handle_duplicates(self):
         """..."""
         # Check for duplicate columns and fix them
