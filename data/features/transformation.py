97889522d726067e4fc3271b87b712675ec07d32
DanielWestgaard <daniel.westgaard@gmail.com>
2025-05-15 15:53:09 +0200

    try catch

--- data/features/transformation.py
+++ data/features/transformation.py
@@ -135,21 +135,23 @@ class FeatureTransformer(BaseProcessor):
     
     def encode_categorical_vars(self):
         """Converting categorical/textual data into numerical format. Like One-hot encoding, label encoding."""
-        categorical_columns = self.df.select_dtypes(include=['object']).columns.tolist()
-        logging.debug(f"Categorical columns about to be one-hot encoded: {categorical_columns}")
-        encoder = OneHotEncoder(sparse_output=False)
+        try:
+            categorical_columns = self.df.select_dtypes(include=['object']).columns.tolist()
+            logging.debug(f"Categorical columns about to be one-hot encoded: {categorical_columns}")
+            encoder = OneHotEncoder(sparse_output=False)
 
-        one_hot_encoded = encoder.fit_transform(self.df[categorical_columns])
-        print(one_hot_encoded)
-        one_hot_df = pd.DataFrame(
-            one_hot_encoded, 
-            columns=encoder.get_feature_names_out(categorical_columns),
-            index=self.df.index  # Both having date as index
-        )
-        df_encoded = pd.concat([self.df, one_hot_df], axis=1)
+            one_hot_encoded = encoder.fit_transform(self.df[categorical_columns])
+            one_hot_df = pd.DataFrame(
+                one_hot_encoded, 
+                columns=encoder.get_feature_names_out(categorical_columns),
+                index=self.df.index  # Both having date as index
+            )
+            df_encoded = pd.concat([self.df, one_hot_df], axis=1)
 
-        self.df = df_encoded.drop(categorical_columns, axis=1)
-        logging.debug(f"Encoded data : \n{self.df}")
+            self.df = df_encoded.drop(categorical_columns, axis=1)
+            logging.debug(f"Encoded data successfully!")
+        except Exception as e:
+            logging.error("Unable to one-hot encode dataset!")
     
     def normalize(self, preserve_original=False, window=20):
         """Perform normalization"""
