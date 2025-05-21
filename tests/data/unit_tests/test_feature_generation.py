f6797ae7cd22763bd01b451d5ea26bec4ee320cc
DanielWestgaard <daniel.westgaard@gmail.com>
2025-05-21 23:35:59 +0200

    just created file

---
+++ tests/data/unit_tests/test_feature_generation.py
@@ -0,0 +1,13 @@
+import unittest
+
+from data.features.generation import FeatureGenerator
+
+
+class TestFeatureGeneration(unittest.TestCase):
+    
+    def setUp(self):
+        """Initialize datasets used for testing."""
+    
+    def test_initialization(self):
+        """Test initialization of FeatureGenerator."""
+        
